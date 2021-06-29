d2d5c8a67ba728b698002d34f90766c412e9ff7f
RozeenStha <73871133+RozeenStha@users.noreply.github.com>
2021-06-29 18:11:41 +0545

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,16 @@ token = os.getenv("DISCORD_BOT_TOKEN")
 async def on_ready():
  print('Bot is ready')
 
+hug1 = "hug me"
+
 @client.event
 async def on_message(message):
     if message.content == "hug":
         await message.channel.send('I will never hug anyone')
+
+@client.event
+async def on_message(message):
+    if message.content == hug1:
+        await message.channel.send('No Never')
 
 client.run(token)
